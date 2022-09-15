0d9c3af25333a242fda72f45de7e0f4fd760701d
Agnes Tseng <tingyun2001@gmail.com>
2022-09-15 23:04:47 +0800

    0915

--- app/test.py
+++ app/test.py
@@ -1,10 +1,181 @@
-from pathlib import Path
-import os
-import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xlrd
+import hashlib
+from pandas import DataFrame
+from collections import Counter
+from openpyxl import load_workbook
+from openpyxl.styles  import Font
+bins_listy=[100,200,300,400,500]
 
-upload_path = os.path.join(os.path.expanduser("~"), 'Downloads')
-os.mkdir(upload_path + '\\' + time.strftime('%Y-%m-%d %H%M', time.localtime())) #在下載裡建立新資料夾
+    #應該是建立新檔案 問
+    #path = 'Q.txt'
+    f = open(app.config["NEW_EXCEL"] + '/new_excel_trademark.xlsx', 'w',encoding='UTF-8')
 
-str_upload_path = str(upload_path)
-print(str_upload_path + "\ori.xlsx")
-print(upload_path)
+    a=pd.read_excel(app.config["EXCEL_UPLOADS"] + '/ori.xlsx', sheet_name='Sheet1')
+    df = pd.DataFrame(a)
+
+    sp = request.args.get('trademark')
+    List= df[sp].tolist()
+
+    df1=df[sp].apply(str)
+    #print(df1)
+    u=[]
+    for i in df1:
+        u.append(i)
+    #print(u)
+
+    T="".join(u)
+    #print(T)
+
+    sha = hashlib.md5(T.encode("utf-8")).hexdigest()
+    print(sha)
+    print(sha,file=f)  
+
+
+    bin_str = ""
+    for n in sha:
+        bin_str += bin(int(n,16))[2:].zfill(4)
+
+    print(bin_str)  #藏入資料
+    print(bin_str,file=f)  
+
+
+    recounted = Counter(List)   #統計資料出現次數
+    #print(recounted)     #檢查
+
+    b1=df.groupby(sp).size()
+    #print(b1)   #檢查
+    A=max(b1)
+    #print("最大次數",A)  #找出最大值的點為多少
+
+    a=np.array(List)   #將資料改成陣列  (分數)
+    a1=np.unique(a)    
+    #print(a1)
+
+    b=np.array(b1)     #將資料轉成陣列  (次數)
+
+    c=np.vstack((b,a1))   #合併變成二維陣列
+    print(c) #檢查
+
+    MAX=max(List)
+    #print(MAX)
+
+    n=-1
+    p=[]
+
+    for row in c:    
+        for col in row: 
+            n=n+1
+            if(col==A):     #判斷次數如果是跟最大一樣
+                #print(c[1][n])
+                p.append(c[1][n])
+
+    M=p[0]     #找第一個出現的最大值
+    print("最高點:",M) 
+
+    ori_file = app.config["EXCEL_UPLOADS"] + '/ori.xlsx'
+    new_file = app.config["NEW_EXCEL"] + '/new_excel_trademark.xlsx'
+
+    m=-1
+    o=[]
+    for i in a:
+        m=m+1  
+        if i < M:  
+            i=i-1
+            df.at[m,sp] = i
+            print(m,i)
+            DataFrame(df).to_excel(ori_file,sheet_name='Sheet1', index=False, header=True)
+            if(i<0): 
+                o.append(m)
+        else:     
+            if(i==M):
+                df.at[m,sp] = i
+                DataFrame(df).to_excel(ori_file,sheet_name='Sheet1', index=False, header=True)
+            else:
+                i=i+1
+                df.at[m,sp] = i
+                DataFrame(df).to_excel(ori_file,sheet_name='Sheet1', index=False, header=True)  
+            
+    print("溢位:",o)
+
+    def union_without_repetition(list1,list2):
+        result = list(set(list1) | set(list2))
+        return result
+
+    r=pd.read_excel(ori_file, sheet_name='Sheet1')
+    sr= df[sp].tolist()
+    ar=np.array(sr)
+
+    bins_listx=union_without_repetition(List,sr)
+    plt.hist(sr,range(0,MAX+2),align='left', edgecolor='#000000',linewidth=2)
+    plt.xlabel('get')
+    plt.ylabel('count')
+    plt.xticks(bins_listx)
+    plt.yticks(bins_listy)
+    #plt.show()
+    plt.savefig("s1.png")
+    plt.close()
+
+    #print(bin_str)  #藏入資料
+    tc=len(bin_str)
+    print("長度:",tc) #128bits
+
+    n=-1
+    t=-1
+    for i in ar:
+        n=n+1
+        if(i==M):
+            t=t+1    #計算第在第幾個資料
+            if(t<tc):
+                if(bin_str[t]=='1'):
+                    i=i-1
+                    df.at[n,sp] = i
+                    DataFrame(df).to_excel(new_file,sheet_name='Sheet1', index=False, header=True)
+                if(bin_str[t]=='0'):
+                    DataFrame(df).to_excel(new_file,sheet_name='Sheet1', index=False, header=True)
+                if(bin_str[t]==' '):
+                    i=i+1
+                    df.at[n,sp] = i
+                    DataFrame(df).to_excel(new_file,sheet_name='Sheet1', index=False, header=True)
+        else:
+            df.at[n,sp]=i
+            DataFrame(df).to_excel(new_file,sheet_name='Sheet1', index=False, header=True)
+
+
+
+    def union_without_repetition(list1,list2):
+        result = list(set(list1) | set(list2))
+        return result
+
+    r=pd.read_excel(new_file)
+    sx= df[sp].tolist()  
+
+    bins_listx=union_without_repetition(List,sr)
+    plt.hist(sx,range(0,MAX+2),align='left', edgecolor='#000000',linewidth=2)
+    plt.xlabel('get')
+    plt.ylabel('count')
+    plt.xticks(bins_listx)
+    plt.yticks(bins_listy)
+    #plt.show()
+    plt.savefig("s2.png")
+
+    wb=load_workbook(filename = 's98.xlsx')
+    sheet_ranges = wb['Sheet1']
+
+    k = pd.read_excel(new_file,nrows=0)
+    L = k.columns.tolist()
+    print(L) 
+    r=L.index(sp)+1
+    #print(r)  #第幾列
+
+    font = Font(size=15)
+
+    for i in o:
+        #print(i) #第幾行
+        i=i+2 #PYTHON讀的格子0 在EXCEL是2
+        f = sheet_ranges.cell(row=i, column=r)
+        f.font = font
+        f.value=0
+        wb.save('Q3.xlsx')
--- app/views.py
+++ app/views.py
@@ -12,10 +12,14 @@ from pandas import DataFrame
 import numpy as np 
 import matplotlib.pyplot as plt 
 from collections import Counter
+import xlrd
+import hashlib
 #用sqlite上傳檔案
 #from . import db
 #用MongoDB上傳檔案
 import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles  import Font
 #import pymongo
 #from pymongo import MongoClient #使用mongodb
 #import certifi #為了解決連線到SSL的問題
@@ -90,8 +94,8 @@ def upload_excel():
 
     return render_template("public/upload_excel.html")
 
-app.config["NEW_EXCEL"] = "/app/app/static/new_excel" #新excel檔的儲存位置
-
+app.config["NEW_EXCEL"] = "/app/app/static/new_excel" #新excel檔(總成績歸零)的儲存位置
+#將總成績清空
 @app.route("/test", methods=["GET", "POST"])
 def test():
 
@@ -120,14 +124,189 @@ def test():
         DataFrame(df).to_excel(app.config["NEW_EXCEL"] + "/" + 'new_excel_test.xlsx', sheet_name='Sheet1', index=False, header=True)
     return redirect("/download/"+'new_excel_test.xlsx') 
 
-
 #下載檔案，用from flask import send_from_directory, abort
 #app.config["CLIENT_EXCELS"] = "/app/app/static/excel" #要從哪裡下載
+
+app.config["EXCEL_TM"] = "/app/app/static/excel_TM" #藏入商標後的檔案存檔路徑
+app.config["EXCEL_SHA"] = "/app/app/static/excel_sha" #雜湊值存檔路徑
+app.config["EXCEL_HIST"] = "/app/app/static/excel_hist" #excel產生的直方圖存檔路徑
+
+#輸入欄位名稱_藏入商標
+@app.route("/add-trademark", methods=["POST"])
+def add_trademark():
+    return render_template("public/addTM.html")
+
+#藏入商標
+@app.route("/trademark", methods=["GET", "POST"])
+def trademark():
+
+    # 使用openpyxl建立新活頁簿wb_new
+    wb_new = Workbook()
+    wb_new.save(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx')
+
+    # 使用openpyxl讀取原始檔案
+    wb = load_workbook(app.config["EXCEL_UPLOADS"] + '/ori.xlsx')
+    ws = wb.worksheets[0]
+
+    # 使用openpyxl讀取new_excel
+    wb_new = load_workbook(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx')
+    ws_new = wb_new.active
+
+    #雜湊值的檔案
+    path = app.config["EXCEL_SHA"] + '/sha.txt'
+    f = open(path, 'w',encoding='UTF-8')
+
+    #讀舊檔，輸入欄位名稱
+    a=pd.read_excel(app.config["EXCEL_UPLOADS"] + '/ori.xlsx')
+    df = pd.DataFrame(a)
+    sp = request.args.get('TMcolname') #request.args.get('TMcolname')是由使用者輸入的欄位名稱
+    List= df[sp].tolist()
+
+    df1=df[sp].apply(str)
+    u=[]
+    for i in df1:
+        u.append(i)
+
+    T="".join(u)
+
+    #生成雜湊值
+    sha = hashlib.md5(T.encode("utf-8")).hexdigest()
+    print(sha,file=f)  #要留 存檔
+
+    bin_str = ""
+    for n in sha:
+        bin_str += bin(int(n,16))[2:].zfill(4)
+    print(bin_str,file=f)  #要留 存檔
+
+    recounted = Counter(List)   #統計資料出現次數
+    b1=df.groupby(sp).size()
+    A=max(b1)  #找出最大值的點為多少
+
+    a=np.array(List)   #將資料改成陣列  (分數)
+    a1=np.unique(a)    
+
+    b=np.array(b1)     #將資料轉成陣列  (次數)
+
+    c=np.vstack((b,a1))   #合併變成二維陣列
+
+    MAX=max(List)
+
+    plt.hist(List,range(0,MAX+2),align='left', edgecolor='#000000',linewidth=2)
+    plt.xticks(List)
+    plt.savefig(app.config["EXCEL_HIST"] + "/hist_ori.png") #原始資料的直方圖
+    plt.close()
+
+    n=-1
+    p=[]
+
+    for row in c:    
+        for col in row: 
+            n=n+1
+            if(col==A):     #判斷次數如果是跟最大一樣
+                p.append(c[1][n])
+
+    M=p[0]     #找第一個出現的最大值
+
+    m=-1
+    o=[]
+    for i in a:
+        m=m+1  
+        if i < M:  
+            i=i-1
+            df.at[m,sp] = i
+            DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+            if(i<0): 
+                o.append(m)
+        else:     
+            if(i==M):
+                df.at[m,sp] = i
+                DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+            else:
+                i=i+1
+                df.at[m,sp] = i
+                DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)  
+
+    def union_without_repetition(list1,list2):
+        result = list(set(list1) | set(list2))
+        return result
+
+    r=pd.read_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1')
+    sr= df[sp].tolist()
+    ar=np.array(sr)
+
+    bins_listx=union_without_repetition(List,sr)
+    plt.hist(sr,range(-1,MAX+2),align='left', edgecolor='#000000',linewidth=2)
+    plt.xticks(bins_listx)
+    plt.savefig(app.config["EXCEL_HIST"] + "/hist_shifting.png")
+    plt.close()
+
+    tc=len(bin_str)
+
+    n=-1
+    t=-1
+    for i in ar:
+        n=n+1
+        if(i==M):
+            t=t+1    #計算第在第幾個資料
+            if(t<tc):
+                if(bin_str[t]=='1'):
+                    i=i-1
+                    df.at[n,sp] = i
+                    DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+                if(bin_str[t]=='0'):
+                    DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+                if(bin_str[t]==' '):
+                    i=i+1
+                    df.at[n,sp] = i
+                    DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+        else:
+            df.at[n,sp]=i
+            DataFrame(df).to_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',sheet_name='Sheet1', index=False, header=True)
+
+
+
+    def union_without_repetition(list1,list2):
+        result = list(set(list1) | set(list2))
+        return result
+
+    fn = app.config["EXCEL_TM"] + '/new_excel_TM.xlsx'
+    wb=load_workbook(fn)
+    sheet_ranges = wb['Sheet1']
+
+    k = pd.read_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx',nrows=0)
+    L = k.columns.tolist()
+    r=L.index(sp)+1
+
+    font = Font(size=15)
+
+    for i in o:
+        i=i+2 #PYTHON讀的格子0 在EXCEL是2
+        f = sheet_ranges.cell(row=i, column=r)
+        f.font = font
+        f.value=0
+        wb.save(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx')
+
+    r=pd.read_excel(app.config["EXCEL_TM"] + '/new_excel_TM.xlsx')
+    sx= df[sp].tolist()
+    bins_listx2=union_without_repetition(bins_listx,sx)
+    plt.hist(sx,range(0,MAX+2),align='left', edgecolor='#000000',linewidth=2)
+    plt.xticks(bins_listx2)
+    plt.savefig(app.config["EXCEL_HIST"] + "/hist_hiding.png")
+
+    return redirect("/download2/"+'new_excel_TM.xlsx')
 
 @app.route("/download/<excel_name>")
 def downloadfile(excel_name):
     try:
         return send_from_directory(app.config["NEW_EXCEL"], path=excel_name, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+#原本的程式碼return send_from_directory(app.config["CLIENT_EXCELS"], filename=excel_name, as_attachment=True)，現在filename要改成path
+
+@app.route("/download2/<excel_name>")
+def downloadfile2(excel_name):
+    try:
+        return send_from_directory(app.config["EXCEL_TM"], path=excel_name, as_attachment=True)
     except FileNotFoundError:
         abort(404)
 #原本的程式碼return send_from_directory(app.config["CLIENT_EXCELS"], filename=excel_name, as_attachment=True)，現在filename要改成path
